1cab4b7b93c4920db7f0238f6624dcf55785ba1a
dail45 <69967573+dail45@users.noreply.github.com>
2020-12-14 08:31:19 +0500

    First Jump

--- main.py
+++ main.py
@@ -14,15 +14,38 @@ class Dino(pygame.sprite.Sprite):
 
         self.image = self.normal
         self.rect = self.image.get_rect()
-        self.y = h / 2 - 30 * 2.2
+        self.rect.x = 15
+        self.y = int(h / 2 - 18 * 2.2)
         self.rect.y = self.y
         self.field = field
         self.last = 0
+        self.jump_possible = 1
+        self.t = 0
+        self.v = 0
+        self.g = 15
+
+    def jump(self):
+        if self.y + 10 >= self.rect.y >= self.y:
+            self.jump_possible = 1
+            self.rect.y = self.y
+            self.t = 0
+            self.v = 0
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            if self.jump_possible:
+                self.v += 130
+            if not self.rect.y > self.y - 10:
+                self.jump_possible = 0
+        else:
+            if self.rect.y != self.y:
+                self.jump_possible = 0
+        self.t += 1
+        self.rect.y = self.y - (self.v * self.t - (self.g * self.t ** 2) / 2) / 100
 
     def update(self):
-        if self.field.rect.x % 64 == 0 and self.field.rect.x != self.last:
+        if -v < self.field.rect.x % 50 <= v and self.field.rect.x != self.last:
             self.change_sprite_tick()
             self.last = self.field.rect.x
+        self.jump()
 
     def change_sprite_tick(self):
         if self.image == self.normal1:
@@ -35,6 +58,20 @@ class Dino(pygame.sprite.Sprite):
             self.image = self.shift1
         elif self.image == self.normal:
             self.image = self.normal1
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, 'Enemy/big_cactus_x3.png'))
+        self.rect = self.image.get_rect()
+        self.rect.y = h / 2 - 40
+        self.rect.x = 1200
+
+    def update(self):
+        self.rect.x -= v
+        if self.rect.x <= -100:
+            self.rect.x = 1200
 
 
 class Field(pygame.sprite.Sprite):
@@ -53,13 +90,14 @@ class Field(pygame.sprite.Sprite):
         global fps
         if self.status:
             self.rect.x -= v
-            if self.rect.x % 80 == 0:
+            if -v < self.rect.x % 50 < v:
+                print(self.rect.x % 50, self.rect.x)
                 self.count += 1
                 print(self.count)
             if fps <= 360 and self.count % 100 == 0 and self.last_speed_up != self.count:
                 fps += 10
                 self.last_speed_up = self.count
-        if self.rect.left <= -2400:
+        if self.rect.left <= -1450:
             self.rect.left = 0
 
     def start(self):
@@ -89,17 +127,15 @@ if __name__ == '__main__':
     dino = Dino(field)
     sprites.add(field)
     sprites.add(dino)
-    v = 8
+    cactus = Enemy()
+    sprites.add(cactus)
+    v = 6
     fps = 100
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    dino.jump()
-                    print(dino.rect.y)
         sprites.update()
         field.render()
         sprites.draw(screen)
